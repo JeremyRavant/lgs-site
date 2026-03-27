1d2ada3efc60db479ab017be99e0a72e9addff32
Jeremy Ravant <jeremy.ravant@outlook.fr>
2026-03-27 19:19:30 +0100

    fix: use getImageUrl in CategorieCard

--- lgs/components/CategorieCard.jsx
+++ lgs/components/CategorieCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import '../components/CategorieCard.scss'
+import { getImageUrl } from "../src/utils/auth";
 
 function CategorieCard({ title, description, cover, index }) {
     const isEven = index % 2 === 0;
   
     return (
       <div id="realisations" className={`card ${isEven ? 'card__left' : 'card__right'}`}>
-        <img src={cover} alt={title} />
+        <img src={getImageUrl(cover)} alt={title} />
         <div className="categorie__title">
           <h2>{title}</h2>
           <p>{description}</p>
